a9f9c6309f6a5e6e377653b0ebbfad5eb51f0e22
ando <hohoku55@yahoo.co.jp>
2017-11-04 20:36:43 +0900

    outputを出力するためのbash(すべてのラベルを３回ずつ実行)

---
+++ bash/runrun.sh
@@ -0,0 +1,107 @@
+#!/bin/sh
+#PBS -N runrun
+#PBS -j oe
+#PBS -q SMALL
+#PBS -l select=8:ncpus=6
+#PBS -V
+
+cd /home/s1610224/worksong/
+python larning.py
+
+
+date
+date >>output
+python random_forest_sample_sm.py travec.txt testvec1.txt >>output
+python random_forest_sample_sm.py travec.txt testvec2.txt >>output
+python random_forest_sample_sm.py travec.txt testvec3.txt >>output
+python random_forest_sample_sm.py travec.txt testvec4.txt >>output
+python random_forest_sample_sm.py travec.txt testvec5.txt >>output
+python random_forest_sample_sm.py travec.txt testvec6.txt >>output
+python random_forest_sample_sm.py travec.txt testvec7.txt >>output
+python random_forest_sample_sm.py travec.txt testvec8.txt >>output
+python random_forest_sample_sm.py travec.txt testvec9.txt >>output
+python random_forest_sample_sm.py travec.txt testvec10.txt >>output
+python random_forest_sample_sm.py travec.txt testvec11.txt >>output
+python random_forest_sample_sm.py travec.txt testvec12.txt >>output
+python random_forest_sample_sm.py travec.txt testvec13.txt >>output
+python random_forest_sample_sm.py travec.txt testvec14.txt >>output
+python random_forest_sample_sm.py travec.txt testvec15.txt >>output
+python random_forest_sample_sm.py travec.txt testvec16.txt >>output
+python random_forest_sample_sm.py travec.txt testvec17.txt >>output
+python random_forest_sample_sm.py travec.txt testvec18.txt >>output
+python random_forest_sample_sm.py travec.txt testvec19.txt >>output
+python random_forest_sample_sm.py travec.txt testvec20.txt >>output
+python random_forest_sample_sm.py travec.txt testvec21.txt >>output
+python random_forest_sample_sm.py travec.txt testvec22.txt >>output
+python random_forest_sample_sm.py travec.txt testvec23.txt >>output
+python random_forest_sample_sm.py travec.txt testvec24.txt >>output
+python random_forest_sample_sm.py travec.txt testvec26.txt >>output
+python random_forest_sample_sm.py travec.txt testvec27.txt >>output
+python random_forest_sample_sm.py travec.txt testvec28.txt >>output
+python random_forest_sample_sm.py travec.txt testvec29.txt >>output
+date
+date >>output
+python random_forest_sample_sm.py travec.txt testvec1.txt >>output
+python random_forest_sample_sm.py travec.txt testvec2.txt >>output
+python random_forest_sample_sm.py travec.txt testvec3.txt >>output
+python random_forest_sample_sm.py travec.txt testvec4.txt >>output
+python random_forest_sample_sm.py travec.txt testvec5.txt >>output
+python random_forest_sample_sm.py travec.txt testvec6.txt >>output
+python random_forest_sample_sm.py travec.txt testvec7.txt >>output
+python random_forest_sample_sm.py travec.txt testvec8.txt >>output
+python random_forest_sample_sm.py travec.txt testvec9.txt >>output
+python random_forest_sample_sm.py travec.txt testvec10.txt >>output
+python random_forest_sample_sm.py travec.txt testvec11.txt >>output
+python random_forest_sample_sm.py travec.txt testvec12.txt >>output
+python random_forest_sample_sm.py travec.txt testvec13.txt >>output
+python random_forest_sample_sm.py travec.txt testvec14.txt >>output
+python random_forest_sample_sm.py travec.txt testvec15.txt >>output
+python random_forest_sample_sm.py travec.txt testvec16.txt >>output
+python random_forest_sample_sm.py travec.txt testvec17.txt >>output
+python random_forest_sample_sm.py travec.txt testvec18.txt >>output
+python random_forest_sample_sm.py travec.txt testvec19.txt >>output
+python random_forest_sample_sm.py travec.txt testvec20.txt >>output
+python random_forest_sample_sm.py travec.txt testvec21.txt >>output
+python random_forest_sample_sm.py travec.txt testvec22.txt >>output
+python random_forest_sample_sm.py travec.txt testvec23.txt >>output
+python random_forest_sample_sm.py travec.txt testvec24.txt >>output
+python random_forest_sample_sm.py travec.txt testvec26.txt >>output
+python random_forest_sample_sm.py travec.txt testvec27.txt >>output
+python random_forest_sample_sm.py travec.txt testvec28.txt >>output
+python random_forest_sample_sm.py travec.txt testvec29.txt >>output
+date
+date >>output
+python random_forest_sample_sm.py travec.txt testvec1.txt >>output
+python random_forest_sample_sm.py travec.txt testvec2.txt >>output
+python random_forest_sample_sm.py travec.txt testvec3.txt >>output
+python random_forest_sample_sm.py travec.txt testvec4.txt >>output
+python random_forest_sample_sm.py travec.txt testvec5.txt >>output
+python random_forest_sample_sm.py travec.txt testvec6.txt >>output
+python random_forest_sample_sm.py travec.txt testvec7.txt >>output
+python random_forest_sample_sm.py travec.txt testvec8.txt >>output
+python random_forest_sample_sm.py travec.txt testvec9.txt >>output
+python random_forest_sample_sm.py travec.txt testvec10.txt >>output
+python random_forest_sample_sm.py travec.txt testvec11.txt >>output
+python random_forest_sample_sm.py travec.txt testvec12.txt >>output
+python random_forest_sample_sm.py travec.txt testvec13.txt >>output
+python random_forest_sample_sm.py travec.txt testvec14.txt >>output
+python random_forest_sample_sm.py travec.txt testvec15.txt >>output
+python random_forest_sample_sm.py travec.txt testvec16.txt >>output
+python random_forest_sample_sm.py travec.txt testvec17.txt >>output
+python random_forest_sample_sm.py travec.txt testvec18.txt >>output
+python random_forest_sample_sm.py travec.txt testvec19.txt >>output
+python random_forest_sample_sm.py travec.txt testvec20.txt >>output
+python random_forest_sample_sm.py travec.txt testvec21.txt >>output
+python random_forest_sample_sm.py travec.txt testvec22.txt >>output
+python random_forest_sample_sm.py travec.txt testvec23.txt >>output
+python random_forest_sample_sm.py travec.txt testvec24.txt >>output
+python random_forest_sample_sm.py travec.txt testvec26.txt >>output
+python random_forest_sample_sm.py travec.txt testvec27.txt >>output
+python random_forest_sample_sm.py travec.txt testvec28.txt >>output
+python random_forest_sample_sm.py travec.txt testvec29.txt >>output
+date
+date >>output
+date
+date >>output
+
+
