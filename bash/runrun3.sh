7a04ebe49f013ef1199351f43abc3319954e5f50
ando <hohoku55@yahoo.co.jp>
2017-11-04 20:37:05 +0900

    output3を出力するためのbash(すべてのラベルを３回ずつ実行)

---
+++ bash/runrun3.sh
@@ -0,0 +1,103 @@
+#!/bin/sh
+#PBS -N runrun3
+#PBS -j oe
+#PBS -q SMALL
+#PBS -l select=8:ncpus=6
+#PBS -V
+
+cd /home/s1610224/worksong/
+python larning3.py
+
+date
+date >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec1.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec2.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec3.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec4.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec5.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec6.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec7.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec8.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec9.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec10.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec11.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec12.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec13.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec14.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec15.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec16.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec17.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec18.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec19.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec20.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec21.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec22.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec23.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec24.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec26.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec27.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec28.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec29.txt >>output3
+date
+date >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec1.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec2.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec3.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec4.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec5.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec6.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec7.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec8.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec9.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec10.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec11.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec12.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec13.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec14.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec15.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec16.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec17.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec18.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec19.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec20.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec21.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec22.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec23.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec24.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec26.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec27.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec28.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec29.txt >>output3
+date
+date >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec1.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec2.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec3.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec4.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec5.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec6.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec7.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec8.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec9.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec10.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec11.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec12.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec13.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec14.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec15.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec16.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec17.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec18.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec19.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec20.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec21.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec22.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec23.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec24.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec26.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec27.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec28.txt >>output3
+python random_forest_sample_sm.py 3travec.txt 3testvec29.txt >>output3
+date
+date >>output3
+
